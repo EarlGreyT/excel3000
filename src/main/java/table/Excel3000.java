2f088f5d4bcd7f9756e8b3b6f349d2d326e3c220
David Weßling <david.wessling@materna.de>
2022-05-10 12:26:46 +0200

    fix: getRowNumber calculates the int matching with eg "AA" correctly;

--- src/main/java/table/Excel3000.java
+++ src/main/java/table/Excel3000.java
@@ -1,21 +1,22 @@
 package table;
 
-import com.google.common.collect.BiMap;
+
 import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.HashBiMap;
 import com.google.common.collect.Table;
-import java.util.Arrays;
+import com.google.common.math.IntMath;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
+
 
 public class Excel3000 {
 
   private Table<Integer, Integer, String> sheet = HashBasedTable.create();
   private static final Map<Character, Integer> rowMapping = new HashMap<>();
   private static final Pattern cellCoordPattern = Pattern.compile("^[A-Z]+[1-9]+$");
+  private static final char refSign ='$';
   private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
   static {
     char[] alphabet = ALPHABET.toCharArray();
     for (int i = 0; i < alphabet.length; i++) {
@@ -23,8 +24,8 @@ public class Excel3000 {
     }
   }
 
-  private static int getRowNumber(String cell) throws IllegalArgumentException{
-    if (!cellCoordPattern.matcher(cell).matches()){
+  private static int getRowNumber(String cell) throws IllegalArgumentException {
+    if (!cellCoordPattern.matcher(cell).matches()) {
       throw new IllegalArgumentException(cell + " could not be matched to a table index");
     }
     int row = 0;
@@ -34,15 +35,19 @@ public class Excel3000 {
         .toString()
         .toCharArray();
     for (int i = 0; i < rowChars.length; i++) {
-      row += rowMapping.get(rowChars[i])+rowMapping.get(ALPHABET.charAt(ALPHABET.length()-1))*i;
+      // decimal 111 = 1*10^2+1*10^1+1*10^0
+      // but our base is the length of the ALPHABET.
+      row += rowMapping.get(rowChars[i]) * IntMath.pow(
+          1 + rowMapping.get(ALPHABET.charAt(ALPHABET.length() - 1)), rowChars.length-(i+1));
     }
     return row;
   }
 
-  public String getCellAt(int row, int col){
-    return sheet.get(row,col);
+  public String getCellAt(int row, int col) {
+    return sheet.get(row, col);
   }
-  static int[] getCoords(String cell){
+
+  static int[] getCoords(String cell) {
     int[] coords = new int[2];
     int row = getRowNumber(cell);
     int col = Integer.parseInt(cell.substring(String.valueOf(row).length()));
@@ -50,18 +55,19 @@ public class Excel3000 {
     coords[1] = col;
     return coords;
   }
-  public String getCellAt(String cell){
+
+  public String getCellAt(String cell) {
     int[] coords = getCoords(cell);
-    return  getCellAt(coords[0], coords[1]);
+    return getCellAt(coords[0], coords[1]);
   }
 
-  public void setCell(int row, int col, String value){
+  public void setCell(int row, int col, String value) {
     sheet.put(row, col, value);
   }
 
-  public void setCell(String cell, String value) throws IllegalArgumentException{
+  public void setCell(String cell, String value) throws IllegalArgumentException {
     int[] coords = getCoords(cell);
-    setCell(coords[0], coords[1],value);
+    setCell(coords[0], coords[1], value);
   }
 
 
