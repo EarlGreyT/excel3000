fd378ad7facd4a18160abd90a0ee91a770bce828
David Weßling <david.wessling@materna.de>
2022-05-11 12:31:03 +0200

    fix: excel does not like "=" in its formulas

--- src/main/java/writer/XlsxWriter.java
+++ src/main/java/writer/XlsxWriter.java
@@ -3,6 +3,7 @@ package writer;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -15,14 +16,18 @@ public class XlsxWriter {
     try (Workbook wb = new XSSFWorkbook(); FileOutputStream fOut = new FileOutputStream(name)) {
       Sheet s = wb.createSheet();
       for (Integer col : table.getSheet().columnKeySet()) {
-        Row r = s.createRow(col-1);
+        Row r = s.createRow(col - 1);
         for (Integer row : table.getSheet().rowKeySet()) {
           Cell c = r.createCell(row);
           String cellValue = table.getCellAt(row, col);
           if (cellValue.chars().allMatch(Character::isDigit) && !cellValue.equals("")) {
             c.setCellValue(Double.parseDouble(cellValue));
           } else {
-            c.setCellValue(cellValue);
+            if (cellValue.startsWith("=")) {
+              c.setCellFormula(cellValue.substring(1));
+            } else {
+              c.setCellValue(cellValue);
+            }
           }
         }
       }
